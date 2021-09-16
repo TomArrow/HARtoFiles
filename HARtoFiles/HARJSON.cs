dcc553973d57cde289b8c8c5d634aea3779c3541
Tom Arrow <davorfilms@googlemail.com>
2021-09-16 22:08:24 +0200

    Add project files.

---
+++ HARtoFiles/HARJSON.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+
+public class Rootobject
+{
+    public Log log { get; set; }
+}
+
+public class Log
+{
+    public string version { get; set; }
+    public Creator creator { get; set; }
+    public Browser browser { get; set; }
+    public Page[] pages { get; set; }
+    public Entry[] entries { get; set; }
+}
+
+public class Creator
+{
+    public string name { get; set; }
+    public string version { get; set; }
+}
+
+public class Browser
+{
+    public string name { get; set; }
+    public string version { get; set; }
+}
+
+public class Page
+{
+    public DateTime startedDateTime { get; set; }
+    public string id { get; set; }
+    public string title { get; set; }
+    public Pagetimings pageTimings { get; set; }
+}
+
+public class Pagetimings
+{
+    public int onContentLoad { get; set; }
+    public int onLoad { get; set; }
+}
+
+public class Entry
+{
+    public string pageref { get; set; }
+    public DateTime startedDateTime { get; set; }
+    public Request request { get; set; }
+    public Response response { get; set; }
+    public Cache cache { get; set; }
+    public Timings timings { get; set; }
+    public int time { get; set; }
+    public string _securityState { get; set; }
+    public string serverIPAddress { get; set; }
+    public string connection { get; set; }
+}
+
+public class Request
+{
+    public int bodySize { get; set; }
+    public string method { get; set; }
+    public string url { get; set; }
+    public string httpVersion { get; set; }
+    public Header[] headers { get; set; }
+    public Cooky[] cookies { get; set; }
+    public Querystring[] queryString { get; set; }
+    public int headersSize { get; set; }
+}
+
+public class Header
+{
+    public string name { get; set; }
+    public string value { get; set; }
+}
+
+public class Cooky
+{
+    public string name { get; set; }
+    public string value { get; set; }
+}
+
+public class Querystring
+{
+    public string name { get; set; }
+    public string value { get; set; }
+}
+
+public class Response
+{
+    public int status { get; set; }
+    public string statusText { get; set; }
+    public string httpVersion { get; set; }
+    public Header1[] headers { get; set; }
+    public object[] cookies { get; set; }
+    public Content content { get; set; }
+    public string redirectURL { get; set; }
+    public int headersSize { get; set; }
+    public int bodySize { get; set; }
+}
+
+public class Content
+{
+    public string mimeType { get; set; }
+    public int size { get; set; }
+    public string encoding { get; set; }
+    public string text { get; set; }
+}
+
+public class Header1
+{
+    public string name { get; set; }
+    public string value { get; set; }
+}
+
+public class Cache
+{
+}
+
+public class Timings
+{
+    public int blocked { get; set; }
+    public int dns { get; set; }
+    public int connect { get; set; }
+    public int ssl { get; set; }
+    public int send { get; set; }
+    public int wait { get; set; }
+    public int receive { get; set; }
+}
---
+++ HARtoFiles/Program.cs
@@ -0,0 +1,152 @@
+﻿using MimeTypes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace HARtoFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                toFiles(arg);
+            }
+            
+
+            Console.ReadKey();
+        }
+        static JsonSerializerOptions opt = new JsonSerializerOptions() { NumberHandling= System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString };
+
+
+        public static object MimeMapping { get; private set; }
+
+        static void toFiles(string sourceHARPath)
+        {
+            string dirname = Path.GetDirectoryName(sourceHARPath);
+            string basename=  (dirname == "" ? "" : dirname+ Path.DirectorySeparatorChar)+ Path.GetFileNameWithoutExtension(sourceHARPath);
+            Directory.CreateDirectory(basename);
+
+            string content = File.ReadAllText(sourceHARPath);
+            Rootobject stuff = JsonSerializer.Deserialize<Rootobject>(content, opt);
+
+            foreach (Entry entry in stuff.log.entries)
+            {
+                // Headers reading
+                Dictionary<string, string> headers = new Dictionary<string, string>();
+                foreach(Header1 header in entry.response.headers)
+                {
+                    //headers.Add(header.name.ToLower(),header.value);
+                    headers[header.name] = header.value;
+                }
+
+                Uri uri = new Uri(entry.request.url);
+                string filename = uri.Segments.Length == 0 ? "__NONAME__" : MakeValidFileName( uri.Segments[uri.Segments.Length-1].Trim().Trim(new char[] { '\\', '/' }));
+                if(filename.Length > 40)
+                {
+                    filename = filename.Substring(0, 40) + "_TRUNCNAME_";
+                }
+                string path = MakeValidFileName(basename);
+                for(int i = 0; i < uri.Segments.Length - 1; i++)
+                {
+                    if(uri.Segments[i] == "/" || uri.Segments[i] == "\\")
+                    {
+                        continue;
+                    }
+                    string pathseg = MakeValidFileName(uri.Segments[i].Trim().Trim(new char[] { '\\', '/' }));
+                    if (pathseg.Length > 40)
+                    {
+                        pathseg = pathseg.Substring(0, 40) + "_TRUNCDIR_";
+                    }
+                    path = Path.Combine(path, pathseg);
+                }
+
+
+                if (!filename.Contains("."))
+                {
+                    string extension = ".raw";
+
+                    if (headers.ContainsKey("content-type"))
+                    {
+                        try {
+
+                            string[] typeParts = headers["content-type"].Split(';');
+                            extension = MimeTypeMap.GetExtension(typeParts[0].Trim());
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine("Extension from content type failed.");
+                        }
+                    }
+
+                    filename = filename + extension;
+                }
+                if (headers.ContainsKey("content-disposition"))
+                {
+                    try {
+
+                        ContentDisposition contentDisposition = new ContentDisposition(headers["content-disposition"]);
+                        if(contentDisposition.FileName != null)
+                        {
+                            filename = contentDisposition.FileName;
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Extension from content type failed.");
+                    }
+                }
+
+                string savePath = Path.Combine(path, filename);
+
+                Directory.CreateDirectory(path);
+
+                savePath = GetUnusedFilename(savePath);
+
+
+
+                if(entry.response.content.encoding != null && entry.response.content.encoding.ToLower() == "base64")
+                {
+
+                    File.WriteAllBytes(savePath, Convert.FromBase64String(entry.response.content.text));
+                } else
+                {
+
+                    File.WriteAllText(savePath, entry.response.content.text);
+                }
+
+                Console.WriteLine(savePath + " written.");
+
+            }
+        }
+
+        public static string GetUnusedFilename(string baseFilename)
+        {
+            if (!File.Exists(baseFilename))
+            {
+                return baseFilename;
+            }
+            string extension = Path.GetExtension(baseFilename);
+
+            int index = 1;
+            while (File.Exists(Path.ChangeExtension(baseFilename, "." + (++index) + extension))) ;
+
+            return Path.ChangeExtension(baseFilename, "." + (index) + extension);
+        }
+
+
+
+
+        // from: https://stackoverflow.com/a/847251
+        public static string MakeValidFileName(string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
+    }
+}
